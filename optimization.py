f0bff6c13b95ef0ece9e8224d63f77ab6eba0b8c
Quinton Austin <121703507+Q-T-A@users.noreply.github.com>
2024-09-08 19:37:53 -0400

    added correct stock tickers for graph

--- optimization.py
+++ optimization.py
@@ -21,9 +21,9 @@ GT honor code violation.
   		  	   		 	   		  		  		    	 		 		   		 		  
 -----do not edit anything above this line---  		  	   		 	   		  		  		    	 		 		   		 		  
   		  	   		 	   		  		  		    	 		 		   		 		  
-Student Name: Tucker Balch (replace with your name)  		  	   		 	   		  		  		    	 		 		   		 		  
-GT User ID: tb34 (replace with your User ID)  		  	   		 	   		  		  		    	 		 		   		 		  
-GT ID: 900897987 (replace with your GT ID)  		  	   		 	   		  		  		    	 		 		   		 		  
+Student Name: Quinton Austin (replace with your name)  		  	   		 	   		  		  		    	 		 		   		 		  
+GT User ID: qaustin3 (replace with your User ID)  		  	   		 	   		  		  		    	 		 		   		 		  
+GT ID: 903616660 (replace with your GT ID)  		  	   		 	   		  		  		    	 		 		   		 		  
 """  		  	   		 	   		  		  		    	 		 		   		 		  
   		  	   		 	   		  		  		    	 		 		   		 		  
   		  	   		 	   		  		  		    	 		 		   		 		  
@@ -33,9 +33,10 @@ import numpy as np
   		  	   		 	   		  		  		    	 		 		   		 		  
 import matplotlib.pyplot as plt  		  	   		 	   		  		  		    	 		 		   		 		  
 import pandas as pd  		  	   		 	   		  		  		    	 		 		   		 		  
-from util import get_data, plot_data  		  	   		 	   		  		  		    	 		 		   		 		  
+from util import get_data, plot_data  	
+from scipy.optimize import minimize	  	   		 	   		  		  		    	 		 		   		 		  
   		  	   		 	   		  		  		    	 		 		   		 		  
-  		  	   		 	   		  		  		    	 		 		   		 		  
+
 # This is the function that will be tested by the autograder  		  	   		 	   		  		  		    	 		 		   		 		  
 # The student must update this code to properly implement the functionality  		  	   		 	   		  		  		    	 		 		   		 		  
 def optimize_portfolio(  		  	   		 	   		  		  		    	 		 		   		 		  
@@ -66,34 +67,59 @@ def optimize_portfolio(
     :rtype: tuple  		  	   		 	   		  		  		    	 		 		   		 		  
     """  		  	   		 	   		  		  		    	 		 		   		 		  
   		  	   		 	   		  		  		    	 		 		   		 		  
-    # Read in adjusted closing prices for given symbols, date range  		  	   		 	   		  		  		    	 		 		   		 		  
+    # Read in adjusted closing prices for given symbols, date range 
+    sv = 100 		  	
+    rfr = 0.0   		   		  		  		    	 		 		   		 		  
     dates = pd.date_range(sd, ed)  		  	   		 	   		  		  		    	 		 		   		 		  
     prices_all = get_data(syms, dates)  # automatically adds SPY  		  	   		 	   		  		  		    	 		 		   		 		  
-    prices = prices_all[syms]  # only portfolio symbols  		  	   		 	   		  		  		    	 		 		   		 		  
-    prices_SPY = prices_all["SPY"]  # only SPY, for comparison later  		  	   		 	   		  		  		    	 		 		   		 		  
-  		  	   		 	   		  		  		    	 		 		   		 		  
+    prices_a = prices_all[syms]  # only portfolio symbols  		
+    prices = prices_a.fillna(method='ffill').fillna(method='bfill')
+  	   		 	   		  		  		    	 		 		   		 		  
+    prices_SPYa = prices_all["SPY"]  # only SPY, for comparison later  		
+    prices_SPY= prices_SPYa.fillna(method='ffill').fillna(method='bfill')
+  	   		 	   		  		  		    	 		 		   		 		  
+    normal = prices / prices.iloc[0]	  	   		 	   		  		  		    	 		 		   		 		  
     # find the allocations for the optimal portfolio  		  	   		 	   		  		  		    	 		 		   		 		  
-    # note that the values here ARE NOT meant to be correct for a test case  		  	   		 	   		  		  		    	 		 		   		 		  
-    allocs = np.asarray(  		  	   		 	   		  		  		    	 		 		   		 		  
-        [0.2, 0.2, 0.3, 0.3]  		  	   		 	   		  		  		    	 		 		   		 		  
-    )  # add code here to find the allocations  		  	   		 	   		  		  		    	 		 		   		 		  
-    cr, adr, sddr, sr = [  		  	   		 	   		  		  		    	 		 		   		 		  
-        0.25,  		  	   		 	   		  		  		    	 		 		   		 		  
-        0.001,  		  	   		 	   		  		  		    	 		 		   		 		  
-        0.0005,  		  	   		 	   		  		  		    	 		 		   		 		  
-        2.1,  		  	   		 	   		  		  		    	 		 		   		 		  
-    ]  # add code here to compute stats  		  	   		 	   		  		  		    	 		 		   		 		  
+    # note that the values here ARE NOT meant to be correct for a test case 
+    def get_negative_sr(allocs):		  	   		 	   		  		  		    	 		 		   		 		  
+        normal = prices / prices.iloc[0]
+        allocated = normal * allocs
+        port_val = allocated.sum(axis=1) * sv
+        port_val_np = port_val.values
+        daily_returns = (port_val_np[1:] / port_val_np[:-1]) - 1
+        cr = (port_val_np[-1] / port_val_np[0]) - 1
+        adr = np.mean(daily_returns)
+        sddr = np.std(daily_returns, ddof = 1)
+        sr = np.sqrt(252) * ((adr - rfr) / sddr)	
+        return -sr
+
+    n = len(syms) #num stocks
+    initial_guess = np.ones(n)/n #guess even
+    bounds = [[0.0, 1.0] for i in range(n)] 
+    constraints = ({'type': 'eq', 'fun' : lambda allocs: np.sum(allocs) - 1})
+    res = minimize(get_negative_sr, initial_guess, bounds = bounds, constraints = constraints)	  	   		 	   		  		  		    	 		 		   		 		  
   		  	   		 	   		  		  		    	 		 		   		 		  
-    # Get daily portfolio value  		  	   		 	   		  		  		    	 		 		   		 		  
-    port_val = prices_SPY  # add code here to compute daily portfolio values  		  	   		 	   		  		  		    	 		 		   		 		  
-  		  	   		 	   		  		  		    	 		 		   		 		  
-    # Compare daily portfolio value with SPY using a normalized plot  		  	   		 	   		  		  		    	 		 		   		 		  
+    allocs = res.x	  
+
+    allocated = normal * allocs	  	   		 	   		  	
+    port_val = allocated.sum(axis=1) * sv
+    port_val_np = port_val.values
+    daily_returns = (port_val_np[1:] / port_val_np[:-1]) - 1
+    cr = (port_val_np[-1] / port_val_np[0]) - 1
+    adr = np.mean(daily_returns)
+    sddr = np.std(daily_returns, ddof = 1)
+    sr = np.sqrt(252) * ((adr - rfr) / sddr)  		    	 		 		   		 		  
     if gen_plot:  		  	   		 	   		  		  		    	 		 		   		 		  
-        # add code to plot here  		  	   		 	   		  		  		    	 		 		   		 		  
-        df_temp = pd.concat(  		  	   		 	   		  		  		    	 		 		   		 		  
-            [port_val, prices_SPY], keys=["Portfolio", "SPY"], axis=1  		  	   		 	   		  		  		    	 		 		   		 		  
-        )  		  	   		 	   		  		  		    	 		 		   		 		  
-        pass  		  	   		 	   		  		  		    	 		 		   		 		  
+        normalized_SPY = prices_SPY / prices_SPY[0]
+        plt.plot(port_val / port_val[0], label="Portfolio")
+        plt.plot(normalized_SPY, label="SPY")
+        plt.title("Optimized Portfolio vs. SPY")
+        plt.xlabel("Date")
+        plt.ylabel("Normalized Value")
+        plt.legend()
+        plt.savefig("./images/Figure1.png")
+        plt.close()
+  		  		    	 		 		   		 		  
   		  	   		 	   		  		  		    	 		 		   		 		  
     return allocs, cr, adr, sddr, sr  		  	   		 	   		  		  		    	 		 		   		 		  
   		  	   		 	   		  		  		    	 		 		   		 		  
@@ -103,13 +129,13 @@ def test_code():
     This function WILL NOT be called by the auto grader.  		  	   		 	   		  		  		    	 		 		   		 		  
     """  		  	   		 	   		  		  		    	 		 		   		 		  
   		  	   		 	   		  		  		    	 		 		   		 		  
-    start_date = dt.datetime(2009, 1, 1)  		  	   		 	   		  		  		    	 		 		   		 		  
-    end_date = dt.datetime(2010, 1, 1)  		  	   		 	   		  		  		    	 		 		   		 		  
-    symbols = ["GOOG", "AAPL", "GLD", "XOM", "IBM"]  		  	   		 	   		  		  		    	 		 		   		 		  
+    start_date = dt.datetime(2008, 6, 1)  		  	   		 	   		  		  		    	 		 		   		 		  
+    end_date = dt.datetime(2009, 6, 1)  		  	   		 	   		  		  		    	 		 		   		 		  
+    symbols = ["IBM", "X", "GLD", "JPM",]  		  	   		 	   		  		  		    	 		 		   		 		  
   		  	   		 	   		  		  		    	 		 		   		 		  
     # Assess the portfolio  		  	   		 	   		  		  		    	 		 		   		 		  
     allocations, cr, adr, sddr, sr = optimize_portfolio(  		  	   		 	   		  		  		    	 		 		   		 		  
-        sd=start_date, ed=end_date, syms=symbols, gen_plot=False  		  	   		 	   		  		  		    	 		 		   		 		  
+        sd=start_date, ed=end_date, syms=symbols, gen_plot=True 		  	   		 	   		  		  		    	 		 		   		 		  
     )  		  	   		 	   		  		  		    	 		 		   		 		  
   		  	   		 	   		  		  		    	 		 		   		 		  
     # Print statistics  		  	   		 	   		  		  		    	 		 		   		 		  
@@ -122,7 +148,18 @@ def test_code():
     print(f"Average Daily Return: {adr}")  		  	   		 	   		  		  		    	 		 		   		 		  
     print(f"Cumulative Return: {cr}")  		  	   		 	   		  		  		    	 		 		   		 		  
   		  	   		 	   		  		  		    	 		 		   		 		  
-  		  	   		 	   		  		  		    	 		 		   		 		  
+def author():  		  	   		 	   		  		  		    	 		 		   		 		  
+    """  		  	   		 	   		  		  		    	 		 		   		 		  
+    :return: The GT username of the student  		  	   		 	   		  		  		    	 		 		   		 		  
+    :rtype: str  		  	   		 	   		  		  		    	 		 		   		 		  
+    """  		  	   		 	   		  		  		    	 		 		   		 		  
+    return "qaustin3"  # replace tb34 with your Georgia Tech username.  		  	   		 	   		  		  		    	 		 		   		 		  
+
+def study_group():
+    
+    return "qaustin3" # replace tb34 with your
+
+
 if __name__ == "__main__":  		  	   		 	   		  		  		    	 		 		   		 		  
     # This code WILL NOT be called by the auto grader  		  	   		 	   		  		  		    	 		 		   		 		  
     # Do not assume that it will be called  		  	   		 	   		  		  		    	 		 		   		 		  
